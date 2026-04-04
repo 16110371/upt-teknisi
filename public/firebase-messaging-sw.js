4ca40820f1ac954b3855f79eb65b26a9548f12dc
Adnan Khatami <adnankhatami10@gmail.com>
2026-04-04 15:14:37 +0700

    file service worker

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDaVcOL_tbf8A5We8n5lzbnAwAF1eVA4Vg",
+    authDomain: "upt-smksw.firebaseapp.com",
+    projectId: "upt-smksw",
+    messagingSenderId: "860712318540",
+    appId: "1:860712318540:web:099cb74eed0a34191326e9"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+    self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body,
+        icon: '/logo.png'
+    });
+});
