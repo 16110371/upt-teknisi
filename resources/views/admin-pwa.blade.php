5006be869fe743205e1c7eff2fe3a628c992de5e
Adnan Khatami <adnankhatami10@gmail.com>
2026-04-05 10:14:04 +0700

    notif v1.7 dev

--- resources/views/admin-pwa.blade.php
+++ resources/views/admin-pwa.blade.php
@@ -5,4 +5,38 @@
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-title" content="UPT Admin">
 
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+
+        // pastikan ini jalan di PWA (iOS)
+        if ('Notification' in window) {
+
+            console.log('Permission awal:', Notification.permission);
+
+            if (Notification.permission === 'default') {
+
+                // iOS BUTUH interaksi user → pakai klik
+                document.body.addEventListener('click', function requestNotifOnce() {
+
+                    Notification.requestPermission().then(permission => {
+                        console.log('Permission:', permission);
+
+                        if (permission === 'granted') {
+                            alert('Notifikasi berhasil diaktifkan 🎉');
+                        } else {
+                            alert('Notifikasi ditolak');
+                        }
+                    });
+
+                    // supaya hanya sekali
+                    document.body.removeEventListener('click', requestNotifOnce);
+                });
+
+            }
+
+        }
+
+    });
+</script>
+
 <link rel="apple-touch-icon" href="/images/admin-icon-192.png">
